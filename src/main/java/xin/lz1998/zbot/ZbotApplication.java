0a12eafc489aeecbfccfa03e4f2cbca84521743d
lz1998 <875543533@qq.com>
2020-04-09 00:06:08 +0800

    first commit

---
+++ src/main/java/xin/lz1998/zbot/ZbotApplication.java
@@ -0,0 +1,13 @@
+package xin.lz1998.zbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xin/lz1998/zbot/ZbotApplicationTests.java
@@ -0,0 +1,13 @@
+package xin.lz1998.zbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
